3d2cdfa273c2087c852ba99312a80ee929ebe46a
Akanksha <mittalakanksha0105@gmail.com>
2026-01-07 23:05:06 +0530

    Problem 12 :Missing Number

---
+++ missingNumber.js
@@ -0,0 +1,15 @@
+const missingNumber = (nums) => {
+	let xor1 = 0,
+		xor2 = 0;
+	let n = nums.length;
+	for (let i = 0; i < nums.length; i++) {
+		xor1 = xor1 ^ nums[i];
+	}
+
+	for (let i = 1; i <= n; i++) {
+		xor2 = xor2 ^ i;
+	}
+
+	return xor1 ^ xor2;
+};
+

8ed04bb09b075818291672c2d14c89b63345887c
Akanksha <mittalakanksha0105@gmail.com>
2026-01-09 23:06:09 +0530

    Problem 14 : Replace Elements With Greatest

--- replaceElementsWithGreatest.js
+++ replaceElementsWithGreatest.js
@@ -1,24 +1,3 @@
-/*
-You are given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
-
-After doing so, return the array.
-
-Example 1:
-
-Input: arr = [2,4,5,3,1,2]
-
-Output: [5,5,3,2,2,-1]
-Example 2:
-
-Input: arr = [3,3]
-
-Output: [3,-1]
-Constraints:
-
-1 <= arr.length <= 10,000
-1 <= arr[i] <= 100,000
-
-*/
 
 const replaceElements = (arr) => {
 	let n = arr.length;
@@ -31,5 +10,3 @@ const replaceElements = (arr) => {
 	return ans;
 };
 
-//T.C. : O(N)
-//S.C. : O(1)
